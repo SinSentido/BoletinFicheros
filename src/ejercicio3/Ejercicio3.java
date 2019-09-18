581af588403ee4a90b7f24d4d422f8fee53c6c1d
SinSentido <sinsentido00@protonmail.com>
2019-09-18 20:04:28 +0200

    ejercicio 3 y 4

--- src/ejercicio3/Ejercicio3.java
+++ src/ejercicio3/Ejercicio3.java
@@ -1,7 +1,50 @@
 package ejercicio3;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Ejercicio3 {
     public static void main(String[] args){
-        
+        Scanner scn = new Scanner(System.in);
+        String dir;
+        File directory;
+
+        System.out.println("Introduce el directorio que quieres mostrar: ");
+        dir = scn.nextLine();
+        directory = new File(dir);
+
+        if(directory.exists()){
+            showFiles(directory);
+        }
+    }
+
+    private static void showFiles (File directory){
+        boolean thereAreDirectories = false;
+        File[] files = directory.listFiles();
+
+        for(int i=0; i<files.length; i++){
+            if(files[i].isDirectory()){
+                thereAreDirectories = true;
+            }
+        }
+
+        if(!thereAreDirectories){
+            for(int i=0; i<files.length; i++){
+                System.out.printf("Nombre: %s%n" +
+                        "Fichero: %b%n" +
+                        "Directorio: %b%n%n", files[i].getName(), files[i].isFile(), files[i].isDirectory());
+            }
+        }
+        else{
+            for(int i=0; i<files.length; i++){
+                System.out.printf("Nombre: %s%n" +
+                        "Fichero: %b%n" +
+                        "Directorio: %b%n%n", files[i].getName(), files[i].isFile(), files[i].isDirectory());
+
+                if(files[i].isDirectory()){
+                    showFiles(files[i]);
+                }
+            }
+        }
     }
 }
---
+++ src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,27 @@
+package ejercicio4;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args){
+        Scanner kbd = new Scanner(System.in);
+        String directorio, extension;
+        String[] ficheros;
+
+        System.out.println("Indica el directorio donde quieres buscar:");
+        directorio = kbd.nextLine();
+
+        System.out.println("Indica la extensión del archivo que quieres buscar:");
+        extension = kbd.nextLine();
+
+        File dir = new File(".", directorio);
+        ficheros = dir.list();
+
+        for(int i=0; i<ficheros.length; i++){
+            if(ficheros[i].matches(".*[.]" + extension)){
+                System.out.println(ficheros[i]);
+            }
+        }
+    }
+}
